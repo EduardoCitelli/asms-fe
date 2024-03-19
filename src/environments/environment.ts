091d643758cc8469c2f1370e5a0434604f9470f3
Eduardo Citelli <eduardo.citelli@hotmail.com>
2024-03-19 19:35:23 -0300

    fix https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://asms-be.somee.com/api/'
+  apiBaseUrl: 'https://asms-be.somee.com/api/'
   // apiBaseUrl: 'https://localhost:7230/api/'
 };
 
